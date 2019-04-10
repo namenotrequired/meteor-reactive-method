189f0d6af4cecc6f0c2d00021300b47df4de38ac
Bart Sturm <bart@tutormundi.com>
2019-04-10 11:58:00 -0300

    Bump version number due to readme update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'bartoftutormundi:reactive-method',
-  version: '1.0.3',
+  version: '1.0.4',
   // Brief, one-line summary of the package.
   summary: 'The old and trusted simple:reactive-method, fixed for meteor 1.8 / lodash 4',
   // URL to the Git repository containing the source code for this package.
